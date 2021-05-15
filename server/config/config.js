053e63737ecfbfeb4e5d22d6c9db9e518cba8ee4
carlos7222 <carlosrincon33@hotmail.com>
2021-05-14 22:16:05 -0500

    Revert "rest server"
    This reverts commit a7d352106a155357ee06e2bac30a9a7869ce0649.

--- server/config/config.js
+++ server/config/config.js
@@ -7,16 +7,3 @@
 //puerto
 //======
 process.env.PORT= process.env.PORT || 3000
-//=======
-//entorno
-//======
-process.env.NODE_ENV= process.env.NODE_ENV || 'dev';
-
-let urldb;
-if(process.env.NODE_ENV=='dev'){
-    urldb='mongodb://localhost:27017/cafe'
-}else{
-    urldb='mongodb+srv://carlos7888:lomaspro85@cluster0.tjpjm.mongodb.net/cafe?retryWrites=true&w=majority'
-}
- 
-process.env.urldb=urldb;
--- server/server.js
+++ server/server.js
@@ -1,26 +1,44 @@
 require('./config/config')
 const express = require('express')
 const app = express()
+const port = 3000
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
- 
-app.use( require('./routes/usurio'));
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
 app.use(bodyParser.json())
 
 
 
 
-mongoose.connect(process.env.URLDB,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true}
-) 
 
+//recibir
+app.get('/usuario', (req, res) => 
+res.json('get usuarios!'))
+//crear
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+     if (body.nombre==undefined){
+        res.status(400).json({
+            ok: false,
+            mensaje: 'el nombre es necesario'
+        });
+     }else{
+        res.json({
+            body:body
+        })
+     }
+}
+)
+//actualizar
+app.put('/usuario/:id', (req, res) =>{
+    let id = req.params.id;
+    res.json({
+        id
+    })
+} 
+  )
+//borrar
+app.delete('/usuario', (req, res) => 
+res.json('delete usuarios!'))
 
 app.listen(process.env.PORT, () => console.log(`Example app listening on port port!`))
